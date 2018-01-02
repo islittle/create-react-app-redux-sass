d5d81cf4bf1b3cc665ec5d8aacfacf48c49446e5
王峰 <wangfeng01@yit.com>
2018-01-02 10:20:58 +0800

    语法错误修改

--- theme.js
+++ theme.js
@@ -2,11 +2,9 @@
 // https://github.com/ant-design/ant-design-mobile/blob/master/components/style/themes/default.less
 // 相应变量值的覆盖
 // 注意：只需写出要覆盖的变量即可（不需要覆盖的变量不要写）
-module.exports = () => {
-  return {
+module.exports = () => ({
     'hd': '2px',
     'primary-color': '#1DA57A',
     'link-color': '#1DA57A',
     'border-radius-base': '2px',
-  };
-};
+});
